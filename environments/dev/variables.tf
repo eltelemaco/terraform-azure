0fdec24132aa3e385b2e6cacb150bbf25033b7c4
Luis Antonio Reynoso <telemaco_81@hotmail.com>
2025-03-10 12:26:23 -0500

    Storage account variable name

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -40,7 +40,7 @@ variable "resource_group_name" {
 variable "storage_account_name" {
   description = "Name of the storage account"
   type        = string
-  default     = "stgdevtfstatemgmt001" #stg<env><name><number>
+  default     = "stgdevtdemomgmt001" #stg<env><name><number>
 }
 
 # Network Configuration
